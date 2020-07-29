ad7859fcae72cbb44199f7fd8fe1c1ff67dd1635
abhiishekporwal <abhiishekporwal@gmail.com>
2020-07-29 16:08:28 +0530

    remove outlier

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,15 @@ def outlierCleaner(predictions, ages, net_worths):
         return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error)
     """
-    
+    import operator 
     cleaned_data = []
+    for i in range(len(predictions)):
+        error = abs(net_worths[i] - predictions[i])
+        cleaned_data.append((ages[i], net_worths[i], error))
 
-    ### your code goes here
-
+    ### your code goes here/
+    cleaned_data.sort(key = operator.itemgetter(2))
+    cleaned_data = cleaned_data[:int(len(predictions)*0.9)]
     
     return cleaned_data
 
