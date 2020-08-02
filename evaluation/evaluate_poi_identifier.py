530c0545b185a85fe7ce44ed3a0060f26933bca8
abhiishekporwal <abhiishekporwal@gmail.com>
2020-08-02 22:31:59 +0530

    validation and evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -2,12 +2,14 @@
 
 
 """
-    starter code for the validation mini-project
-    the first step toward building your POI identifier!
+    starter code for the evaluation mini-project
+    start by copying your trained/tested POI identifier from
+    that you built in the validation mini-project
+
+    the second step toward building your POI identifier!
 
     start by loading/formatting the data
 
-    after that, it's not our code anymore--it's yours!
 """
 
 import pickle
@@ -15,14 +17,17 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from sklearn.model_selection import train_test_split
-
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+### add more features to features_list!
 features_list = ["poi", "salary"]
 
+# data = featureFormat(data_dict, features_list)
+# labels, features = targetFeatureSplit(data)
+
+
+
+### your code goes here 
 data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
@@ -37,3 +42,18 @@ pred = clf.predict(test_split)
 accuracy = accuracy_score(pred, test_labels)
 print(accuracy)
 
+from collections import Counter
+count = Counter(pred)
+print(count[1])
+
+test_numbers = len(test_split)
+print(test_numbers)
+
+count_for_real_poi = Counter(test_labels)
+count_for_real_poi_1=  count_for_real_poi[1]
+print('if all the predicted labels are 0', (test_numbers - count_for_real_poi_1)/test_numbers)
+
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+print('Recall_score: ', recall_score(test_labels, pred))
+print('Precision_score: ',precision_score(test_labels, pred))
